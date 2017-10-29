69cc4eb02a6daac6870821e8e49e6dbb5e201827
Amritanshu1 <33184412+Amritanshu1@users.noreply.github.com>
2017-10-29 23:00:40 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,23 @@
+class Demo
+{
+
+public static void main(String[] args)
+{
+System.out.println("Hello java");
+  //obj1();
+method.obj4();
+method.obj5();
+}
+
+public static void obj1()
+{
+System.out.println("kya partner:");
+ obj2();
+}
+
+public static void obj2()
+{
+System.out.println("coming partner:");
+}
+
+}
